d0ccafdf6737e42e37c54e2424567925310d74da
Sreejith Nair <sreejith190nair@gmail.com>
2023-03-24 07:35:57 +0530

    mongodb & mongoose

--- natours/controllers/tourController.js
+++ natours/controllers/tourController.js
@@ -1,19 +1,4 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkId = (req, res, next, val) => {
-  const id = req.params.id * 1;
-  if (id > tours.length) {
-    return res.status(404).json({
-      status: 'error',
-      message: 'Tour Not Found',
-    });
-  }
-  next();
-}
+const Tour = require('./../models/tourModel')
 
 exports.validate = (req, res, next) => {
   if(!(req.body.name && req.body.price)){
@@ -21,17 +6,17 @@ exports.validate = (req, res, next) => {
       status: 'error',
       message: 'Field is required',
     });
-  }
+  } 
   next();
 }
 
 exports.get_tours = (req, res) => {
   res.status(200).json({
     status: 'success',
-    results: tours.length,
-    data: {
-      tours,
-    },
+    // results: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
@@ -42,9 +27,9 @@ exports.get_tour = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedTime: req.requestedTime,
-    data: {
-      tour,
-    },
+    // data: {
+    //   tour,
+    // },
   });
 };
 
@@ -52,18 +37,11 @@ exports.create_tour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
-  tours.push(newTour);
-
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      res.status(201).send({
-        status: 'success',
-        data: {
-          tour: newTour,
-        },
-      });
+  res.status(201).send({
+    status: 'success',
+    data: {
+      tour: newTour,
+    },
     }
   );
 };
--- natours/server.js
+++ natours/server.js
@@ -1,9 +1,31 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
 dotenv.config({path:'./config.env'});
 
+const db = process.env.DATABASE;
+
+mongoose
+.connect(db,{
+    useNewUrlParser:true,
+    // useCreateIndex:true,
+    // useFindAndModify:false
+})
+.then(() =>  console.log('DB connection successful'))
+
+
+
+// const testTour = new Tour ({
+//     name:"The Forest Hiker",
+//     rating:4.6,
+//     price:232
+// })
+// testTour
+//     .save()
+//     .then(doc => console.log(doc))
+//     .catch(err => console.log("error : ", err));
 
 const app = require('./app');
-
 
 const port = process.env.PORT || 6000;
 app.listen(port, () => console.log(`App is running on port ${port}`));
