1aa1963faa4678c9640ea9d31b88005efd18a859
Sreejith Nair <sreejith190nair@gmail.com>
2023-05-07 16:14:18 +0530

    debugging and error handling

--- natours/app.js
+++ natours/app.js
@@ -5,20 +5,32 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+const notFound = (req, res, next) => {
+  const err = new Error(`Can't find ${req.originalUrl} not found`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+};
+
+const errHandler = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const status = err.status || 'Something went wrong';
+
+  res.status(statusCode).json({
+    status,
+    message: err.message,
+  });
+};
 
 // Middlewares
 if (process.env.NODE_ENV == 'development') {
-  app.use(morgan('dev')); 
+  app.use(morgan('dev'));
 }
 
 app.use(express.json());
 
-app.use(express.static(`${__dirname}/public/`))
+app.use(express.static(`${__dirname}/public/`));
 
-// app.use((req, res, next) => {
-//   console.log('UsingMiddleware');
-//   next();
-// });
 
 app.use((req, res, next) => {
   req.requestedTime = new Date().toISOString();
@@ -27,7 +39,11 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.use('/api/v1/tours', tourRouter); 
+app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-module.exports = app; 
+app.all('*', notFound);
+
+app.use(errHandler);
+
+module.exports = app;
